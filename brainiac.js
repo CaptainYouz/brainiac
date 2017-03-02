c58ca8a68a96a175bed64256a2c79c31eb1bd042
Anas Mammeri <anas.mammeri@gmail.com>
2017-03-02 15:49:14 +0100

    feat: adding of setup and write websocket routes, with verification of parameters from Brainiac

--- brainiac.js
+++ brainiac.js
@@ -6,6 +6,17 @@
 var exec = require('child_process').exec;
 
 var Brainiac = {
+  verification: {
+    getMode: function(mode) {
+      if (mode === 'in' || mode === 'input') return 'in';
+      if (mode === 'out' || mode === 'output') return 'out';
+      return false;
+    },
+    getPinValue: function(pinValue) {
+      let parsedPinValue = parseInt(pinValue);
+      return (parsedPinValue === 0 || parsedPinValue === 1) ? parsedPinValue : false;
+    }
+  },
   setup: function(pin, mode) {
     exec('gpio -g mode ' + pin + ' ' + mode);
   },
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var server = require('http').createServer(app);
 var io     = require('socket.io')(server);
 var router = require('express').Router();
 
+var Brainiac = require('./brainiac.js');
+
 router.get('/', function(req, res) {
   res.status(200).json(
     {
@@ -14,6 +16,34 @@ router.get('/', function(req, res) {
 
 io.on('connection', function(client) {
   console.log('New connexion of: ', client.id);
+
+  client.on('setup', function(datas) {
+    console.log('setup datas:', datas);
+
+    if (typeof(datas) === 'object') {
+      if (!Array.isArray(datas)) datas = [datas];
+
+      datas.forEach(function(data) {
+        var mode = Brainiac.verification.getMode(data.mode);
+
+        if (mode) Brainiac.setup(data.pinNumber, mode);
+      });
+    }
+  });
+
+  client.on('write', function(datas) {
+    console.log('write datas:', datas);
+
+    if (typeof(datas) === 'object') {
+      if (!Array.isArray(datas)) datas = [datas];
+
+      datas.forEach(function(data) {
+        var pinValue = Brainiac.verification.getPinValue(data.value);
+
+        if (pinValue === 0 || pinValue === 1) Brainiac.write(data.pinNumber, pinValue);
+      });
+    }
+  });
 });
 
 app.use('/', router);
